96536a53611dfa60dc61232d24387e31d7c7b774
otto-menza <otoniel.jump@gmail.com>
2026-08-03 15:39:19 -0500

    Create Login component, with a template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { ListadoProductos } from './listado-productos/listado-productos';
 import { Formulario } from './formulario/formulario';
 import { Error404 } from './error/error';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     {path:'', component: ListadoProductos},
     {path:'listado', component: ListadoProductos},
     {path:'agregar', component: Formulario},
     {path: 'editar/:llave', component: Formulario},
+    {path: 'login', component: Login},
     //Ruta comodin:
     {path: '**', component: Error404}
 ];
